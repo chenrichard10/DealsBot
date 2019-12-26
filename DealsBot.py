d4a4df6325048815677b5864b76a104e73de937a
Richard Chen <40343579+chenrichard10@users.noreply.github.com>
2019-12-25 21:18:24 -0500

    Reddit post searching works now

--- DealsBot.py
+++ DealsBot.py
@@ -2,147 +2,163 @@
 """
 Created on Sun Dec 22 11:47:38 2019
 
-@author: chenp
+@Author Richard Chen 
 """
-import os 
+import os
 import discord
 from discord.ext import commands
-import praw 
+import praw
 from dotenv import load_dotenv
-from bs4 import BeautifulSoup as soup 
+from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as req
 
+description = '''A redflagdeals discord bot'''
+bot = commands.Bot(command_prefix='$', description=description)
 
-reddit = praw.Reddit(client_id='OtBVnyEJ_YCE-g', client_secret='x9Lq_Nk4XnX1tSbZ7_vVrJ5U3ck', user_agent='RedditWebScraping')
-subreddit=''
+reddit = praw.Reddit(client_id='OtBVnyEJ_YCE-g',
+                     client_secret='x9Lq_Nk4XnX1tSbZ7_vVrJ5U3ck', user_agent='RedditWebScraping')
+subr = ''
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_GUILD")
 
 client = discord.Client()
 text_channel_list = []
+# stores user post_listing from RFD
 user_posts = []
+# stores time of user_post from RFD
 time = []
-hot_posts = []
-#used to trakc the very first post
+# used to see if a redflagdeals post is the very first post
 counter = 0
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-    # for General discord servers 
+    print(f'{bot.user} has connected to Discord!')
+    # for General discord servers, takes the first channel from the server
     '''for guild in client.guilds:
         for channel in guild.channels:
             if channel.type == discord.ChannelType.text:
                 text_channel_list.append(channel)'''
 
-    #for friend's server 
-    mychannel = client.get_channel(659095120104259626)
+    # for friend's server
+    mychannel = bot.get_channel(649883813513527296)
+
+    # await mychannel.send('DealsBot is online! Would you like to seach the latest deals? Send $hot deals to see the latest deals on red flag deals forum')
+    await mychannel.send('DealsBot is online! Would you like to seach the latest deals? Command prefix is **$**. To display the latest deals from redflagdeals forums use **$hot_deals**. To view the current subreddit that will be searched, enter **$current_sub**. To update the current sub, use **$enter_sub** followed by the desired subreddit separated by a space. To search the subreddit for top 10 hot posts, use **$hot_posts**')
 
 
-    
-    print(text_channel_list)
-    #await mychannel.send('DealsBot is online! Would you like to seach the latest deals? Send $hot deals to see the latest deals on red flag deals forum')
-    await mychannel.send('DealsBot is online! Would you like to seach the latest deals? Send $hot deals to see the latest deals on red flag deals forum'
-                        +'\nSend $currentsub to view current subreddit, $entersub to change the subreddit, and $hotposts to see 10 hot posts')
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def bot_help(ctx):
+    await ctx.send('To display the latest deals from redflagdeals forums use **$hot_deals**. To view the current subreddit that will be searched, enter **$current_sub**. To update the current sub, use **$enter_sub** followed by the desired subreddit separated by a space. To search the subreddit for top 10 hot posts, use **$hot_posts**')
 
-    if message.content.startswith('$hot deals'):
-        user_posts=[]
-        my_url= "https://forums.redflagdeals.com/hot-deals-f9/" 
-        # Opening up connection, grabbing the page
-        uClient = req(my_url)
-        #Stores the content into a variable
-        page_html= uClient.read()
-        #Close the content 
-        uClient.close()
-        #Html parser
-        page_soup= soup(page_html,"html.parser") 
-        # Searching html with all divs with class "thread_info_title"
-        deals = page_soup.findAll("div",{"class":"thread_info_title"}) 
 
-        for index,value in enumerate(deals):
-            user_string = "\n".join(user_posts)
-            
-            if (index==0):
-                continue 
-            
-            elif (len(user_string)) > 1800:
-                global counter
-                if  counter == 0:
-                    user_string = "__**Red Flag Deals Forum Hot Deals:**__\n\n" +user_string+"** **"+"\n"
-                    await message.channel.send(user_string) 
-                    user_posts = ["\n"]
-                    counter = 1
-                    continue 
-                    
-                else:
-                    user_string = user_string+"\n"
-                    await message.channel.send(user_string)
-                    user_posts= ["\n"]
-                    continue 
-                    
+@bot.command()
+async def hot_deals(ctx):
+    user_posts = []
+    my_url = "https://forums.redflagdeals.com/hot-deals-f9/"
+    # Opening up connection, grabbing the page
+    uClient = req(my_url)
+    # Stores the content into a variable
+    page_html = uClient.read()
+    # Close the content
+    uClient.close()
+    # Html parser
+    page_soup = soup(page_html, "html.parser")
+    # Searching html with all divs with class "thread_info_title"
+    deals = page_soup.findAll("div", {"class": "thread_info_title"})
 
-                
+    for index, value in enumerate(deals):
+        user_string = "\n".join(user_posts)
+
+        if (index == 0):
+            continue
+
+        elif (len(user_string)) > 1800:
+            global counter
+            if counter == 0:
+                user_string = "__**Red Flag Deals Forum Hot Deals:**__\n\n" + user_string+"** **"+"\n"
+                await ctx.send(user_string)
+                user_posts = ["\n"]
+                counter = 1
+                continue
+
             else:
-                score = deals[index].findAll("span",{"class":"total_count"})
-                score = score[0].text
-                retailer = deals[index].findAll("a",{"class":"topictitle_retailer"})
-                time = deals[index].findAll("ul",{"class":"thread-meta-small"})
-                time = time[0].li.text.replace('\n','')
-                sale = deals[index].findAll("a",{"class":"topic_title_link"}) 
-                sale = sale[0].text.replace('\n','')
-                #checks for an empty list 
-                if score == '':
-                    score = "+0 SCORE"
-                if not retailer:
-                    retailer = deals[index].findAll("h3",{"class":"topictitle"})
-                    retailer = retailer[0].text.replace('\n','')
-                    if retailer == sale and score =='':
-                        score = "**"+"+0 SCORE"+"**"
-                        user_posts.append(score + " "+ time+" "+"**"+retailer+"**"+"\n")
-                    elif score== '':
-                        score = "**"+"+0 SCORE"+"**"
-                        user_posts.append(score+" "+time+" "+"**"+retailer+"**"+"\n")
-                    #elif retailer == sale:
-                    #    user_posts.append("**"+score+"**"+" "+" "+time+" " +"**"+ retailer+"**"+"\n")
-                    else:
-                        user_posts.append("**"+score+"**"+" "+" "+time+" " +"**"+ retailer+"**"+"\n")
-                elif score == '':
-                    retailer = retailer[0].text.replace('\n','')
-                    user_posts.append(time+" " +"**"+retailer+ "**"+" "+ "**"+sale+"**"+"\n")
-                else:
-                    retailer = retailer[0].text.replace('\n','')
-                    user_posts.append("**"+score+"**"+" "+time+" " +"**"+retailer+ "**"+" "+ "**"+sale+"**"+"\n") 
-        
-        user_posts= "\n".join(user_posts)
-        
-        await message.channel.send(user_posts)
-        counter = 0
-        return
-    
-    elif message.content.startswith('$currentsub'):
-        global subreddit
-        global hot_posts
-        if subreddit=='':
-            await message.channel.send('no specified subreddit at the moment.')
-        else:
-            await message.channel.send(subreddit)
-    elif message.content.startswith('$entersub'):
-        await message.channel.send('Please enter your subreddit')
+                user_string = user_string+"\n"
+                await ctx.send(user_string)
+                user_posts = ["\n"]
+                continue
 
-        def check(m):
-            return m.content
-        subreddit = await client.wait_for('message',check=check)
-        if subreddit:
-            subreddit= subreddit.content
-            hot_posts = reddit.subreddit(subreddit).hot(limit=10)
-            for post in hot_posts:
-                hot_posts.append("**Upvotes:"+str(post.score)+"** "+"**"+post.title+"**"+"\n")
-            hot_posts= "\n".join(hot_posts)
-            await message.channel.send(hot_posts)
-client.run(TOKEN)
+        else:
+            score = deals[index].findAll("span", {"class": "total_count"})
+            score = score[0].text
+            retailer = deals[index].findAll(
+                "a", {"class": "topictitle_retailer"})
+            time = deals[index].findAll("ul", {"class": "thread-meta-small"})
+            time = time[0].li.text.replace('\n', '')
+            sale = deals[index].findAll("a", {"class": "topic_title_link"})
+            sale = sale[0].text.replace('\n', '')
+            # checks for an empty list
+            if score == '':
+                score = "+0 SCORE"
+            if not retailer:
+                retailer = deals[index].findAll("h3", {"class": "topictitle"})
+                retailer = retailer[0].text.replace('\n', '')
+                if retailer == sale and score == '':
+                    score = "**"+"+0 SCORE"+"**"
+                    user_posts.append(score + " " + time +
+                                      " "+"**"+retailer+"**"+"\n")
+                elif score == '':
+                    score = "**"+"+0 SCORE"+"**"
+                    user_posts.append(score+" "+time+" " +
+                                      "**"+retailer+"**"+"\n")
+
+                else:
+                    user_posts.append("**"+score+"**"+" " +
+                                      " "+time+" " + "**" + retailer+"**"+"\n")
+            elif score == '':
+                retailer = retailer[0].text.replace('\n', '')
+                user_posts.append(time+" " + "**"+retailer +
+                                  "**"+" " + "**"+sale+"**"+"\n")
+            else:
+                retailer = retailer[0].text.replace('\n', '')
+                user_posts.append("**"+score+"**"+" "+time+" " +
+                                  "**"+retailer + "**"+" " + "**"+sale+"**"+"\n")
+
+    user_posts = "\n".join(user_posts)
+
+    await ctx.send(user_posts)
+    counter = 0
+    return
+
+
+@bot.command()
+async def enter_sub(ctx, sub: str):
+    global subr
+    subr = sub
+    await ctx.send('Current subreddit: ' + '**'+subr+'** ')
+
+
+@bot.command()
+async def current_sub(ctx):
+    global subr
+    if subr == '':
+        await ctx.send('No specified subreddit at the moment.')
+    else:
+        await ctx.send(subr)
+
+
+@bot.command()
+async def hot_posts(ctx):
+    global subr
+    a = []
+    hot1 = reddit.subreddit(subr).hot(limit=10)
+    for post in hot1:
+        a.append("**Upvotes: "+str(post.score)+"** " +
+                 "**"+post.title+"**"+"\n"+post.url+"\n")
+
+    a = "\n".join(a)
+    a = '__**Hot Posts from ' + subr + '**__\n\n' + a
+    await ctx.send(a)
+
+bot.run('NjU3NzA0NTg4MDIyOTA2OTEw.XgPKwA.o3QJ24HIHU4Ifn7k9FM40Z3FIkk')
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # DealsBot
-A discord bot to display latest sales from redflagdeals forum
+DealsBot is a bot which allows users to view the latest deals on the hot deals forums section of RedFlagDeals as well as view top 10 hot posts on any specified subreddit on reddit.com. Although the reddit section was meant for subreddits such as r/buildapcsalescanada, it still has the functionality to view any subreddit's top 10 hot posts
+
+# Requirements
+* Python 3.5.3 or higher 
+* Discord.py and BeautifulSoup 
+* Bot Token provided by Creator 
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+
