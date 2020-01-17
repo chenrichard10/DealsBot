e3f4794963b4b0170aa2b416faf4da80cf4e540e
Richard Chen <40343579+chenrichard10@users.noreply.github.com>
2020-01-17 17:30:19 -0500

    Small changes to code

--- DealsBot.py
+++ DealsBot.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+    # -*- coding: utf-8 -*-
 """
 Created on Sun Dec 22 11:47:38 2019
 
@@ -31,6 +31,69 @@ time = []
 # used to see if a redflagdeals post is the very first post
 counter = 0
 
+#Adds a new line to eveyr string
+def join_strings(curr_string):
+    return "\n".join(curr_string)
+
+# Checks character limit of discord bot message
+def exceed_char_limit(curr_string):
+    if (len(curr_string)>=1800):
+        return True
+    else:
+        return False 
+
+# Adds Header from forum 
+def add_header(curr_string,counter):
+    if counter == 0:
+        return "__**Red Flag Deals Forum Hot Deals:**__\n\n" + curr_string+"** **"+"\n"
+    else:
+        return curr_string
+
+# Finds the score of a deal
+def find_score(deal,index):
+    return deal[index].findAll("span", {"class": "total_count"})[0].text
+
+# Finds retailer of a deal
+def find_retailer(deal,index):
+    return deal[index].findAll("a", {"class": "topictitle_retailer"})
+
+# Finds time of a deal 
+def find_time(deal,index):
+    return deal[index].findAll("ul", {"class": "thread-meta-small"})[0].li.text.replace('\n', '')
+
+# Finds the title of a deal
+def find_sales(deal,index):
+    return deal[index].findAll("a", {"class": "topic_title_link"})[0].text.replace('\n', '')
+
+# Formatting and recreating the post
+def create_post(deals,index,user_posts):
+    score = find_score(deals,index)
+    retailer = find_retailer(deals,index)
+    time = find_time(deals,index)
+    sale = find_sales(deals,index)
+    # checks for an empty list
+    if score == '':
+        score = "+0 SCORE"
+    if not retailer:
+        retailer = deals[index].findAll("h3", {"class": "topictitle"})
+        retailer = retailer[0].text.replace('\n', '')
+        if retailer == sale and score == '':
+            score = "**"+"+0 SCORE"+"**"
+            user_posts.append(score + " " + time +" "+"**"+retailer+"**"+"\n")
+        elif score == '':
+            score = "**"+"+0 SCORE"+"**"
+            user_posts.append(score+" "+time+" " +"**"+retailer+"**"+"\n")
+
+        else:
+            user_posts.append("**"+score+"**"+" " +" "+time+" " + "**" + retailer+"**"+"\n")
+    elif score == '':
+        retailer = retailer[0].text.replace('\n', '')
+        user_posts.append(time+" " + "**"+retailer +"**"+" " + "**"+sale+"**"+"\n")
+    else:
+        retailer = retailer[0].text.replace('\n', '')
+        user_posts.append("**"+score+"**"+" "+time+" " +"**"+retailer + "**"+" " + "**"+sale+"**"+"\n")
+    return user_posts
+
 
 @bot.event
 async def on_ready():
@@ -42,7 +105,7 @@ async def on_ready():
                 text_channel_list.append(channel)'''
 
     # for friend's server
-    mychannel = bot.get_channel(649883813513527296)
+    mychannel = bot.get_channel(659095120104259626)
 
     # await mychannel.send('DealsBot is online! Would you like to seach the latest deals? Send $hot deals to see the latest deals on red flag deals forum')
     await mychannel.send('DealsBot is online! Would you like to seach the latest deals? Command prefix is **$**. To display the latest deals from redflagdeals forums use **$hot_deals**. To view the current subreddit that will be searched, enter **$current_sub**. To update the current sub, use **$enter_sub** followed by the desired subreddit separated by a space. To search the subreddit for top 10 hot posts, use **$hot_posts**')
@@ -69,64 +132,23 @@ async def hot_deals(ctx):
     deals = page_soup.findAll("div", {"class": "thread_info_title"})
 
     for index, value in enumerate(deals):
-        user_string = "\n".join(user_posts)
+        global counter
+        user_string = add_header(join_strings(user_posts), counter)
 
         if (index == 0):
             continue
 
-        elif (len(user_string)) > 1800:
-            global counter
-            if counter == 0:
-                user_string = "__**Red Flag Deals Forum Hot Deals:**__\n\n" + user_string+"** **"+"\n"
-                await ctx.send(user_string)
-                user_posts = ["\n"]
-                counter = 1
-                continue
-
-            else:
-                user_string = user_string+"\n"
-                await ctx.send(user_string)
-                user_posts = ["\n"]
-                continue
+        elif exceed_char_limit(user_string):
+            await ctx.send(user_string)
+            user_posts = ["\n"]
+            #adds 1 to the counter 
+            counter +=1;
+            continue
 
         else:
-            score = deals[index].findAll("span", {"class": "total_count"})
-            score = score[0].text
-            retailer = deals[index].findAll(
-                "a", {"class": "topictitle_retailer"})
-            time = deals[index].findAll("ul", {"class": "thread-meta-small"})
-            time = time[0].li.text.replace('\n', '')
-            sale = deals[index].findAll("a", {"class": "topic_title_link"})
-            sale = sale[0].text.replace('\n', '')
-            # checks for an empty list
-            if score == '':
-                score = "+0 SCORE"
-            if not retailer:
-                retailer = deals[index].findAll("h3", {"class": "topictitle"})
-                retailer = retailer[0].text.replace('\n', '')
-                if retailer == sale and score == '':
-                    score = "**"+"+0 SCORE"+"**"
-                    user_posts.append(score + " " + time +
-                                      " "+"**"+retailer+"**"+"\n")
-                elif score == '':
-                    score = "**"+"+0 SCORE"+"**"
-                    user_posts.append(score+" "+time+" " +
-                                      "**"+retailer+"**"+"\n")
+            user_posts = create_post(deals,index,user_posts)
 
-                else:
-                    user_posts.append("**"+score+"**"+" " +
-                                      " "+time+" " + "**" + retailer+"**"+"\n")
-            elif score == '':
-                retailer = retailer[0].text.replace('\n', '')
-                user_posts.append(time+" " + "**"+retailer +
-                                  "**"+" " + "**"+sale+"**"+"\n")
-            else:
-                retailer = retailer[0].text.replace('\n', '')
-                user_posts.append("**"+score+"**"+" "+time+" " +
-                                  "**"+retailer + "**"+" " + "**"+sale+"**"+"\n")
-
-    user_posts = "\n".join(user_posts)
-
+    user_posts = join_strings(user_posts)
     await ctx.send(user_posts)
     counter = 0
     return
@@ -155,13 +177,6 @@ async def hot_posts(ctx):
     hot1 = reddit.subreddit(subr).hot(limit=10)
     await ctx.send('__**Hot Posts from ' + subr + "**__"+ "** **"+"\n" )
     for post in hot1:
-       # a.append("**Upvotes: "+str(post.score)+"** " +
-                # "**"+post.title+"**"+"\n"+post.url+"\n")
-        await ctx.send("** **"+"\n"+"**Upvotes: "+str(post.score)+"** " +
-                       "**"+post.title+"**"+"\n"+post.url+"\n" )
-
-   # a = "\n".join(a)
-   # a = '__**Hot Posts from ' + subr + '**__\n\n' + a
-    #await ctx.send(a)
+        await ctx.send("** **"+"\n"+"**Upvotes: "+str(post.score)+"** " +"**"+post.title+"**"+"\n"+post.url+"\n" )
 
 bot.run(TOKEN)
